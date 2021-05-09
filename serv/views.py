37ae155690705e7b269edd97e40f49719c5d529d
Priyanka Biswas <priyankabiswasonly1@gmail.com>
2021-05-09 12:23:03 +0530

    firstCommit

---
+++ serv/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def service(request):
+    context = {'service': 'active'}
+    return render(request, 'serv/service.html', context)
